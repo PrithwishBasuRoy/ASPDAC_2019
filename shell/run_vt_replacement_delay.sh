702647661152f304de5e28fe24bf4f4b38993a8d
Prithwish <prithwishpb@gmail.com>
2019-07-31 21:29:27 +0530

    shell for running the arrival time of vt_replacement. It gives you the critical path

---
+++ shell/run_vt_replacement_delay.sh
@@ -0,0 +1,2 @@
+g++ -g -std=c++0x vt_gate_replacemet_delay.cpp -o near_crit_path_test.out
+./near_crit_path_test.out aes_128.hgr aes_128.type aes_128.fanout aes_128.fanin lvt_gate_lookup hvt_gate_lookup aes_128.rise_m_02_02_5_5 aes_128.fall_m_02_02_5_5 hvt_cell_leakage_power lvt_cell_leakage_power parity_combined > dump_critical
